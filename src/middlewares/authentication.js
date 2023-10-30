a5b5c2c98161a86747158e5e53fde92cadc52f09
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-30 21:41:19 +0100

    authentication.js

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -8,14 +8,19 @@ const User = require('../models/user')
 const Token = require('../models/token')
 
 module.exports = async (req, res, next) => {
+  // Authorization: Token ...
+  // Authorization: ApiKey ...
+  // Authorization: X-API-KEY ...
+  // Authorization: x-auth-token ...
+  // Authorization: Bearer ...
 
-    const auth = req.headers?.authorization || null
-    const tokenKey = auth ? auth.split(' ')[1] : null
+  const auth = req.headers?.authorization || null; // Token ...tokenKey...
+  const tokenKey = auth ? auth.split(" ")[1] : null; // ['Token', '...tokenKey...']
 
-    if (tokenKey) {
-        const tokenData = await Token.findOne({ token: tokenKey })
-        if (tokenData) req.user = await User.findOne({ _id: tokenData.userId })
-    }   
+  if (tokenKey && tokenKey[0] == "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] });
+    if (tokenData) req.user = await User.findOne({ _id: tokenData.userId });
+  }
 
-    next()
+  next();
 }
